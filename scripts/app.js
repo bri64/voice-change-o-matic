ce7370d083c98f85ce7fa20a77652a6c6e1ae9b1
Brian <brianstrains@gmail.com>
2021-02-09 23:18:13 -0500

    Frequency reader

--- scripts/app.js
+++ scripts/app.js
@@ -4,7 +4,7 @@ document.body.addEventListener('click', init);
 
 
 function init() {
-  heading.textContent = 'Voice-change-O-matic';
+  heading.textContent = 'Tuner';
   document.body.removeEventListener('click', init)
 
   // Older browsers might not implement mediaDevices at all, so we set an empty object first
@@ -50,59 +50,16 @@ function init() {
   var mute = document.querySelector('.mute');
 
   //set up the different audio nodes we will use for the app
+  var gainNode = audioCtx.createGain();
 
   var analyser = audioCtx.createAnalyser();
   analyser.minDecibels = -90;
   analyser.maxDecibels = -10;
   analyser.smoothingTimeConstant = 0.85;
 
-  var distortion = audioCtx.createWaveShaper();
-  var gainNode = audioCtx.createGain();
-  var biquadFilter = audioCtx.createBiquadFilter();
-  var convolver = audioCtx.createConvolver();
-
-  // distortion curve for the waveshaper, thanks to Kevin Ennis
-  // http://stackoverflow.com/questions/22312841/waveshaper-node-in-webaudio-how-to-emulate-distortion
-
-  function makeDistortionCurve(amount) {
-    var k = typeof amount === 'number' ? amount : 50,
-      n_samples = 44100,
-      curve = new Float32Array(n_samples),
-      deg = Math.PI / 180,
-      i = 0,
-      x;
-    for ( ; i < n_samples; ++i ) {
-      x = i * 2 / n_samples - 1;
-      curve[i] = ( 3 + k ) * x * 20 * deg / ( Math.PI + k * Math.abs(x) );
-    }
-    return curve;
-  };
-
   // grab audio track via XHR for convolver node
 
   var soundSource;
-
-  ajaxRequest = new XMLHttpRequest();
-
-  ajaxRequest.open('GET', 'https://mdn.github.io/voice-change-o-matic/audio/concert-crowd.ogg', true);
-
-  ajaxRequest.responseType = 'arraybuffer';
-
-
-  ajaxRequest.onload = function() {
-    var audioData = ajaxRequest.response;
-
-    audioCtx.decodeAudioData(audioData, function(buffer) {
-        soundSource = audioCtx.createBufferSource();
-        convolver.buffer = buffer;
-      }, function(e){ console.log("Error with decoding audio data" + e.err);});
-
-    //soundSource.connect(audioCtx.destination);
-    //soundSource.loop = true;
-    //soundSource.start();
-  };
-
-  ajaxRequest.send();
 
   // set up canvas context for visualizer
 
@@ -126,15 +83,10 @@ function init() {
         .then(
           function(stream) {
              source = audioCtx.createMediaStreamSource(stream);
-             source.connect(distortion);
-             distortion.connect(biquadFilter);
-             biquadFilter.connect(gainNode);
-             convolver.connect(gainNode);
              gainNode.connect(analyser);
              analyser.connect(audioCtx.destination);
 
           	 visualize();
-             voiceChange();
         })
         .catch( function(err) { console.log('The following gUM error occured: ' + err);})
   } else {
@@ -145,56 +97,6 @@ function init() {
     WIDTH = canvas.width;
     HEIGHT = canvas.height;
 
-
-    var visualSetting = visualSelect.value;
-    console.log(visualSetting);
-
-    if(visualSetting === "sinewave") {
-      analyser.fftSize = 2048;
-      var bufferLength = analyser.fftSize;
-      console.log(bufferLength);
-      var dataArray = new Uint8Array(bufferLength);
-
-      canvasCtx.clearRect(0, 0, WIDTH, HEIGHT);
-
-      var draw = function() {
-
-        drawVisual = requestAnimationFrame(draw);
-
-        analyser.getByteTimeDomainData(dataArray);
-
-        canvasCtx.fillStyle = 'rgb(200, 200, 200)';
-        canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
-
-        canvasCtx.lineWidth = 2;
-        canvasCtx.strokeStyle = 'rgb(0, 0, 0)';
-
-        canvasCtx.beginPath();
-
-        var sliceWidth = WIDTH * 1.0 / bufferLength;
-        var x = 0;
-
-        for(var i = 0; i < bufferLength; i++) {
-
-          var v = dataArray[i] / 128.0;
-          var y = v * HEIGHT/2;
-
-          if(i === 0) {
-            canvasCtx.moveTo(x, y);
-          } else {
-            canvasCtx.lineTo(x, y);
-          }
-
-          x += sliceWidth;
-        }
-
-        canvasCtx.lineTo(canvas.width, canvas.height/2);
-        canvasCtx.stroke();
-      };
-
-      draw();
-
-    } else if(visualSetting == "frequencybars") {
       analyser.fftSize = 256;
       var bufferLengthAlt = analyser.frequencyBinCount;
       console.log(bufferLengthAlt);
@@ -216,6 +118,7 @@ function init() {
 
         for(var i = 0; i < bufferLengthAlt; i++) {
           barHeight = dataArrayAlt[i];
+          console.log(barHeight);
 
           canvasCtx.fillStyle = 'rgb(' + (barHeight+100) + ',50,50)';
           canvasCtx.fillRect(x,HEIGHT-barHeight/2,barWidth,barHeight/2);
@@ -226,52 +129,7 @@ function init() {
 
       drawAlt();
 
-    } else if(visualSetting == "off") {
-      canvasCtx.clearRect(0, 0, WIDTH, HEIGHT);
-      canvasCtx.fillStyle = "red";
-      canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
-    }
-
   }
-
-  function voiceChange() {
-
-    distortion.oversample = '4x';
-    biquadFilter.gain.setTargetAtTime(0, audioCtx.currentTime, 0)
-
-    var voiceSetting = voiceSelect.value;
-    console.log(voiceSetting);
-
-    //when convolver is selected it is connected back into the audio path
-    if(voiceSetting == "convolver") {
-      biquadFilter.disconnect(0);
-      biquadFilter.connect(convolver);
-    } else {
-      biquadFilter.disconnect(0);
-      biquadFilter.connect(gainNode);
-
-      if(voiceSetting == "distortion") {
-        distortion.curve = makeDistortionCurve(400);
-      } else if(voiceSetting == "biquad") {
-        biquadFilter.type = "lowshelf";
-        biquadFilter.frequency.setTargetAtTime(1000, audioCtx.currentTime, 0)
-        biquadFilter.gain.setTargetAtTime(25, audioCtx.currentTime, 0)
-      } else if(voiceSetting == "off") {
-        console.log("Voice settings turned off");
-      }
-    }
-  }
-
-  // event listeners to change visualize and voice settings
-
-  visualSelect.onchange = function() {
-    window.cancelAnimationFrame(drawVisual);
-    visualize();
-  };
-
-  voiceSelect.onchange = function() {
-    voiceChange();
-  };
 
   mute.onclick = voiceMute;
 
